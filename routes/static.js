c87b34114f13d3aa0467d1e3ef1727f78f446dd1
CatsRun <97203917+CatsRun@users.noreply.github.com>
2025-01-15 12:18:46 -0500

    working

--- routes/static.js
+++ routes/static.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use(express.static("public"))
+router.use("/css", express.static(__dirname + "public/css"))
+router.use("/js", express.static(__dirname + "public/js"))
+router.use("/images", express.static(__dirname + "public/images"))
 
-module.exports = router;
+// Exports the router object, along with all of these use statements for use in other areas of the application. This is VERY IMPORTANT. If a resource is NOT exported, it cannot be used somewhere else.
+module.exports = router
 
 
 
